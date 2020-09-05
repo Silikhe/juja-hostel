2b752583efd0fccfc1d381366b5ddb43529f75ba
Sil silas <silaswans@gmail.com>
2020-09-06 02:57:17 +0300

    recyclerview

--- src/context.js
+++ src/context.js
@@ -116,8 +116,6 @@ class RoomProvider extends Component {
             type,
             capacity,
             price,
-            minPrice,
-            maxPrice,
             minSize,
             maxSize,
             breakfast,
